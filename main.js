9883a1937f227f1fdbcfd234d58934792f975b7b
Vitali <apanel@bk.ru>
2022-09-17 13:34:50 +0300

    add calc

---
+++ main.js
@@ -0,0 +1,20 @@
+function calc(operation, firstnumber, secondnumber) {
+    if(!operation || !firstnumber || !secondnumber || typeof firstnumber !== 'number' || typeof secondnumber !== 'number') {
+        return 'Error';
+    }
+    if(operation === 'sum') {
+        return firstnumber + secondnumber;
+    } else if(operation === 'sub') {
+        return firstnumber - secondnumber;
+    } else if(operation === 'mult') {
+        return firstnumber * secondnumber;
+    } else if(operation === 'div') {
+        return firstnumber / secondnumber;
+    } else if(operation === 'Math.pow') {
+        return firstnumber ** secondnumber;
+    } else {
+        return 'unknown operation';
+    }
+}
+console.log(calc('sum', 3, 2));
+
